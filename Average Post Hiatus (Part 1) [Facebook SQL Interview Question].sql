3c540385f1c51209cd70c864aca7d934bff7a334
GANESHKAKADE24 <113114746+GANESHKAKADE24@users.noreply.github.com>
2022-12-30 23:35:32 +0530

    Update Average Post Hiatus (Part 1) [Facebook SQL Interview Question].sql

--- Average Post Hiatus (Part 1) [Facebook SQL Interview Question].sql
+++ Average Post Hiatus (Part 1) [Facebook SQL Interview Question].sql
@@ -1,4 +1,4 @@
-/ * Question Link- https://datalemur.com/questions/sql-average-post-hiatus-1 
+/* Question Link- https://datalemur.com/questions/sql-average-post-hiatus-1 
 
 Given a table of Facebook posts, for each user who posted at least twice in 2021, write a query to find the number of days between each user’s first post of the year and last post of the year in the year 2021. Output the user and number of the days between each user's first and last post.
 
